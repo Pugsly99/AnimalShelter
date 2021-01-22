bcab0f8250d23fbd1970bf4b0b842fc220455077
Pugsly99 <rylandadams@yahoo.com>
2021-01-22 08:51:15 -0800

    cat dog Models

---
+++ AnimalShelter/Models/Cat.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelter.Models
+{
+    public class Cat
+    {
+        public int CatId { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string Birthday { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ AnimalShelter/Models/Dog.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelter.Models
+{
+    public class Dog
+    {
+        public int DogId { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string Birthday { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
