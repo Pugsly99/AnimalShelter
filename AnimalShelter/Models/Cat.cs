5a98c67954ab4c3758e1d72d2f6346e823cea6af
Pugsly99 <rylandadams@yahoo.com>
2021-01-22 10:43:25 -0800

    Required inputs

--- AnimalShelter/Models/Cat.cs
+++ AnimalShelter/Models/Cat.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 namespace AnimalShelter.Models
 {
     public class Cat
     {
         public int CatId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Species { get; set; }
+        [Required]
+        [Range(1, 99, ErrorMessage = "Age must be between 1 and 99.")]
         public int Age { get; set; }
+        [Required]
         public string Gender { get; set; }
     }
 }
