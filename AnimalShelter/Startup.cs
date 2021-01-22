d714fd7ac237d2b5336a3a39ee1845a07db630ef
Pugsly99 <rylandadams@yahoo.com>
2021-01-22 14:13:16 -0800

    comment out versioning

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using AnimalShelter.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using AnimalShelter.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Reflection;
+using System;
 using System.IO;
 
 namespace AnimalShelter
@@ -20,16 +21,21 @@ namespace AnimalShelter
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // services.AddApiVersioning(o => {
+            //     o.ReportApiVersions = true;
+            //     o.UseApiBehavior = false;
+            //     o.AssumeDefaultVersionWhenUnspecified = true;
+            //     o.DefaultApiVersion = new ApiVersion(1, 0);
+            // });
             services.AddSwaggerGen();
+            
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -38,7 +44,6 @@ namespace AnimalShelter
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseSwagger();
