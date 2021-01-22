2d926c87744804ec2e8f7462d478cb1e631b5c5d
Pugsly99 <rylandadams@yahoo.com>
2021-01-22 09:46:05 -0800

    Cat Dog controllers

---
+++ AnimalShelter/Controllers/CatsController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CatsController : ControllerBase
+  {
+    private AnimalShelterContext _db;
+
+    public CatsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/cats
+    [HttpGet]
+    public ActionResult<IEnumerable<Cat>> Get()
+    {
+      return _db.Cats.ToList();
+    }
+    
+    [HttpGet("{id}")]
+    public ActionResult<Cat> Get(int id)
+    {
+        return _db.Cats.FirstOrDefault(entry => entry.CatId == id);
+    }
+
+
+    // POST api/cats
+    [HttpPost]
+    public void Post([FromBody] Cat cat)
+    {
+      _db.Cats.Add(cat);
+      _db.SaveChanges();
+    }
+  }
+}
---
+++ AnimalShelter/Controllers/DogsController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DogsController : ControllerBase
+  {
+    private AnimalShelterContext _db;
+
+    public DogsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<Dog>> Get()
+    {
+      return _db.Dogs.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Dog> Get(int id)
+    {
+        return _db.Dogs.FirstOrDefault(entry => entry.DogId == id);
+    }
+
+    // POST api/dogs
+    [HttpPost]
+    public void Post([FromBody] Dog dog)
+    {
+      _db.Dogs.Add(dog);
+      _db.SaveChanges();
+    }
+  }
+}
