3307d226343c42919d7fb595376b813f3b6f9003
Pugsly99 <rylandadams@yahoo.com>
2021-01-22 16:00:00 -0800

    new schema

--- AnimalShelter/Migrations/20210122235830_Initial.cs
+++ AnimalShelter/Migrations/20210122235830_Initial.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace AnimalShelter.Migrations
 {
-    public partial class Intial : Migration
+    public partial class Initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -13,10 +13,10 @@ namespace AnimalShelter.Migrations
                 {
                     CatId = table.Column<int>(nullable: false)
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    Name = table.Column<string>(nullable: true),
-                    Species = table.Column<string>(nullable: true),
-                    Age = table.Column<int>(nullable: false),
-                    Gender = table.Column<string>(nullable: true)
+                    Name = table.Column<string>(nullable: false),
+                    Species = table.Column<string>(nullable: false),
+                    Age = table.Column<string>(nullable: false),
+                    Gender = table.Column<string>(nullable: false)
                 },
                 constraints: table =>
                 {
@@ -29,10 +29,10 @@ namespace AnimalShelter.Migrations
                 {
                     DogId = table.Column<int>(nullable: false)
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    Name = table.Column<string>(nullable: true),
-                    Species = table.Column<string>(nullable: true),
-                    Age = table.Column<int>(nullable: false),
-                    Gender = table.Column<string>(nullable: true)
+                    Name = table.Column<string>(nullable: false),
+                    Species = table.Column<string>(nullable: false),
+                    Age = table.Column<string>(nullable: false),
+                    Gender = table.Column<string>(nullable: false)
                 },
                 constraints: table =>
                 {
@@ -44,11 +44,16 @@ namespace AnimalShelter.Migrations
                 columns: new[] { "CatId", "Age", "Gender", "Name", "Species" },
                 values: new object[,]
                 {
-                    { 1, 2, "Male", "Pippen", "Persian" },
-                    { 2, 1, "Female", "May", "Bengal" },
-                    { 3, 1, "Female", "Nala", "Siamese" },
-                    { 4, 3, "Male", "Niko", "Ragdoll" },
-                    { 5, 4, "Female", "Simba", "Maine Coon" }
+                    { 1, "2", "Male", "Pippen", "Persian" },
+                    { 9, "3", "Male", "Snuffs", "Ragdoll" },
+                    { 8, "1", "Female", "Jay", "Siamese" },
+                    { 7, "6", "Female", "Puki", "Bengal" },
+                    { 6, "6", "Male", "Mittens", "Persian" },
+                    { 10, "1", "Female", "Kinko", "Maine Coon" },
+                    { 4, "3", "Male", "Niko", "Ragdoll" },
+                    { 3, "1", "Female", "Nala", "Siamese" },
+                    { 2, "1", "Female", "May", "Bengal" },
+                    { 5, "4", "Female", "Simba", "Maine Coon" }
                 });
 
             migrationBuilder.InsertData(
@@ -56,11 +61,16 @@ namespace AnimalShelter.Migrations
                 columns: new[] { "DogId", "Age", "Gender", "Name", "Species" },
                 values: new object[,]
                 {
-                    { 1, 4, "Male", "Leith", "Beagle" },
-                    { 2, 1, "Female", "Marry", "Poodle" },
-                    { 3, 6, "Female", "Nakita", "German Shepherd" },
-                    { 4, 7, "Male", "Apollo", "Siberian Husky" },
-                    { 5, 5, "Female", "Ava", "German Shepherd" }
+                    { 9, "3", "Male", "Mr.Pebbles", "Siberian Husky" },
+                    { 1, "4", "Male", "Leith", "Beagle" },
+                    { 2, "1", "Female", "Marry", "Poodle" },
+                    { 3, "6", "Female", "Nakita", "German Shepherd" },
+                    { 4, "7", "Male", "Apollo", "Siberian Husky" },
+                    { 5, "5", "Female", "Ava", "German Shepherd" },
+                    { 6, "4", "Male", "Link", "Beagle" },
+                    { 7, "5", "Female", "Larry", "Poodle" },
+                    { 8, "7", "Female", "Tiger", "German Shepherd" },
+                    { 10, "3", "male", "Rex", "German Shepherd" }
                 });
         }
 
